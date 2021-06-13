87f7f7a78e92e5fc657f4d32dbf5f143a22a2ba7
Danila Kuntsevich <pro_voda@bk.ru>
2021-06-13 23:22:09 +0500

    homework2

--- mysite/user_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('login/', views.user_login, name="login" ),
-    path('logout/', views.user_logout, name = "logout"),
-    path('registration/', views.user_registration, name="registration")
-]
